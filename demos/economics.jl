9afd981def80656092f603e3bae34e348b507b2e
Ola Yeku <ooyeku@gmail.com>
2023-09-17 23:35:54 -0500

    added show environment function

---
+++ demos/economics.jl
@@ -0,0 +1,56 @@
+include("../lib/Essentials/src/Essentials.jl")
+using .Essentials
+
+install_tidyverse()
+install_janitor()
+load_tidyverse()
+load_janitor()
+
+df = load_economics()
+
+example01 = begin 
+    # What is the avg unemployment rate?
+    println("What is the avg unemployment rate?")
+    mean(df[!, :unemploy])
+end
+
+example02 = begin 
+    # what is the total change in population?
+    println("what is the total change in population?")
+    sum(df[!, :pop])
+end
+
+example03 = begin 
+    # what is the average change y/y change in population?
+    println("what is the average change y/y change in population?")
+    mean(df[!, :psavert])
+end
+
+example04 = begin 
+    # what is the average change y/y change in unemployment?
+    println("what is the average change y/y change in unemployment?")
+    mean(df[!, :uempmed])
+end
+
+example05 = begin
+    # what corolation is there between the change in unemployment and the change in population?
+    println("what corolation is there between the change in unemployment and the change in population?")
+    cor(df[!, :uempmed], df[!, :pop])
+end
+
+
+example06 = begin 
+    # create a model using the change in unemployment to predict the change in population
+    println("create a model using the change in unemployment to predict the change in population")
+    f(x) = cor(df[!, :uempmed], df[!, :pop]) * x
+    test = f.(df[!, :uempmed])
+    df[!, :predicted_uempmed] = test
+    df
+    # create loss function
+    loss(x) = sum((df[!, :pop] .- x).^2)
+    println("loss: ", loss(test))
+    # find the best fit
+    best_fit = argmin(loss, -100:100)
+    println("best fit: ", best_fit)
+    println("loss: ", loss(best_fit))
+end
